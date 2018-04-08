7092e9c0a5943000cbbb1646b1589ab56dd860d4
Eric Baltutis <baltu008@umn.edu>
2018-04-08 15:27:01 -0500

    organize into folders and used javascript for buttons

---
+++ assets/javascript/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    $('#hangmanBtn').on("click", function () {
+        window.open("https://baltu008.github.io/Hangman");
+    });
+
+    $('#crystalBtn').on("click", function () {
+        window.open("https://baltu008.github.io/week-4-game");
+    });
+
+    // $('#Btn').on("click", function () {
+    //     window.open("#");
+    // });
+
+    // $('#Btn').on("click", function () {
+    //     window.open("#");
+    // });
+
+    // $('#Btn').on("click", function () {
+    //     window.open("#");
+    // });
+    
+    // $('#Btn').on("click", function () {
+    //     window.open("#");
+    // });
+
+});
+
+
